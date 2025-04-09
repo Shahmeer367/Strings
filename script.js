69cb77433f6fa0fe1bf4b312af4bd918beaf12ee
SHAHMEER <shahmeerrehmani9@gmail.com>
2025-04-08 22:31:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let a = "Shahmeer"
+let b = "Rehmani"
+
+//  finding character
+console.log(a.charAt(0));
+console.log(a.at(1));
+console.log(a[2]);
+
+//  character code
+console.log(a.charCodeAt(1));
+
+//  string length
+console.log(a.length);
+
+//  change string to lowercase and uppercase
+console.log(a.toLowerCase());
+console.log(a.toUpperCase());
+
+//  slicing string
+console.log(a.slice(2, 6));
+console.log(a.slice(3));
+
+//  cancating two string
+console.log(a.concat(b));
+
+//  replacing string character
+console.log(a.replace("ee", "i"));
